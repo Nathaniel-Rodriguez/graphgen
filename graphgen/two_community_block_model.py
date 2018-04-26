8e248cc5a647b2fd719b4184f0e399b674ad0a80
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2018-04-25 23:01:54 -0400

    changed two community generator to use numpy random states

--- graphgen/two_community_block_model.py
+++ graphgen/two_community_block_model.py
@@ -3,13 +3,10 @@ Generates weighted or unweighted graphs using a block model.
 """
 
 import networkx as nx 
-import numpy as np 
-import scipy.stats as stats
-import sys
-import random as rnd
+import numpy as np
 
 
-def unweighted_two_community_graph(N, mu, avg_degree):
+def unweighted_two_community_graph(random_state, N, mu, avg_degree):
     """
     To create the graph we generate a poisson distributed number of edges for each 
     group pair where the mean is w_rs  (1/2 w_rs when r=s). Then we assign each end
@@ -22,8 +19,8 @@ def unweighted_two_community_graph(N, mu, avg_degree):
     A = 2.0 * avg_degree / N - B # On diagonal probability
 
     # Random group member assignment
-    nodes = set(xrange(N))
-    community1 = set(rnd.sample(nodes, N/2))
+    nodes = set(range(N))
+    community1 = set(random_state.random_sample(nodes, N/2))
     community2 = nodes.difference(community1)
     graph = nx.DiGraph()
     graph.add_nodes_from(nodes)
@@ -34,8 +31,8 @@ def unweighted_two_community_graph(N, mu, avg_degree):
             graph.node[node]['community'] = 2
 
     # Generate edges
-    num_edges_com1tocom2, num_edges_com2tocom1 = np.random.poisson(B * (N/2.0)**2, size=2)
-    num_edges_com1tocom1, num_edges_com2tocom2 = np.random.poisson(A * (N/2.0)**2, size=2)
+    num_edges_com1tocom2, num_edges_com2tocom1 = random_state.poisson(B * (N/2.0)**2, size=2)
+    num_edges_com1tocom1, num_edges_com2tocom2 = random_state.poisson(A * (N/2.0)**2, size=2)
 
     add_edges_to_graph(graph, community1, community2, num_edges_com1tocom2)
     add_edges_to_graph(graph, community2, community1, num_edges_com2tocom1)
@@ -45,16 +42,16 @@ def unweighted_two_community_graph(N, mu, avg_degree):
     return graph
 
 
-def add_edges_to_graph(graph, com1_set, com2_set, num_edges_to_add):
+def add_edges_to_graph(graph, com1_set, com2_set, num_edges_to_add, random_state):
     com1_set = list(com1_set)
     com2_set = list(com2_set)
-    for i in xrange(num_edges_to_add):
-        tail = rnd.choice(com1_set)
-        head = rnd.choice(com2_set)
+    for i in range(num_edges_to_add):
+        tail = random_state.choice(com1_set)
+        head = random_state.choice(com2_set)
         tries = 0
         while ( (graph.has_edge(tail, head) or (tail==head)) and (tries < 10)):
-            tail = rnd.choice(com1_set)
-            head = rnd.choice(com2_set)
+            tail = random_state.choice(com1_set)
+            head = random_state.choice(com2_set)
             tries += 1
 
         if tries >= 10:
@@ -63,24 +60,26 @@ def add_edges_to_graph(graph, com1_set, com2_set, num_edges_to_add):
             graph.add_edge(tail, head)
 
 
-def uniform_weighted_two_community_graph(N, mu, avg_degree, lower_bound=0, upper_bound=1):
+def uniform_weighted_two_community_graph(random_state, N, mu, avg_degree, lower_bound=0,
+                                         upper_bound=1):
 
-    graph = unweighted_two_community_graph(N, mu, avg_degree)
-    weights = np.random.uniform(lower_bound, upper_bound, size=nx.number_of_edges(graph))
+    graph = unweighted_two_community_graph(random_state, N, mu, avg_degree)
+    weights = random_state.uniform(lower_bound, upper_bound, size=nx.number_of_edges(graph))
     for i, edge in enumerate(graph.edges_iter()):
         graph[edge[0]][edge[1]]['weight'] = weights[i]
 
     return graph
 
 
-def gamma_weighted_two_community_graph(N, mu, avg_degree, EE_W=30000.0, negative_weights=False):
+def gamma_weighted_two_community_graph(random_state, N, mu, avg_degree, EE_W=30000.0,
+                                       negative_weights=False):
 
-    graph = unweighted_two_community_graph(N, mu, avg_degree)
+    graph = unweighted_two_community_graph(random_state, N, mu, avg_degree)
 
-    weights = random_gamma(EE_W, nx.number_of_edges(graph))
+    weights = random_gamma(random_state, EE_W, nx.number_of_edges(graph))
 
     for i, edge in enumerate(graph.edges_iter()):
-        if rnd.random() < 0.5:
+        if random_state.random() < 0.5:
             graph[edge[0]][edge[1]]['weight'] = weights[i]
         else:
             graph[edge[0]][edge[1]]['weight'] = -weights[i]
@@ -88,7 +87,7 @@ def gamma_weighted_two_community_graph(N, mu, avg_degree, EE_W=30000.0, negative
     return graph
 
 
-def random_gamma(mean, size=1, shape=4.0):
+def random_gamma(random_state, mean, size=1, shape=4.0):
     """
     Determines the appropriate gamma parameters for a given mean and 
     standard deviation, then returns an array of randomly generated
@@ -100,12 +99,12 @@ def random_gamma(mean, size=1, shape=4.0):
     if mean > 0:
         shape = shape
         scale = mean / shape
-        return stats.gamma.rvs(shape, size=size, loc=0.0, scale=scale)
+        return random_state.gamma(shape, size=size, loc=0.0, scale=scale)
     else:
         mean = -1. * mean
         shape = shape
         scale = mean / shape
-        return -1. * stats.gamma.rvs(shape, size=size, loc=0.0, scale=scale)
+        return -1. * random_state.gamma(shape, size=size, loc=0.0, scale=scale)
 
 
 if __name__ == '__main__':
